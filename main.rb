345cf31c6c7c406291155cb44a58900f5fa2c4de
Artem Kostiuk <postatum@gmail.com>
2018-07-04 11:00:28 +0300

    Fix parsers picking

--- main.rb
+++ main.rb
@@ -2,25 +2,19 @@ require_relative './utils'
 require_relative './parsers'
 
 
-PARSERS = {
-  "brujula" => method(:brujula_parse),
-  "ramlrb" => method(:ramlrb_parse)
-}
+def parse_with(parser_name, fpath)
+  case parser_name
+  when "brujula"
+    brujula_parse(fpath)
+  when "ramlrb"
+    ramlrb_parse(fpath)
+  end
+end
 
 def main()
   options = OptParse.parse(ARGV)
   puts options.verbose
   puts options.parser
-
-  # case options.parser
-  # when "brujula"
-  #   parser_func = lambda { |x| brujula_parse(x) }
-  # when "ramlbr"
-  #   parser_func = lambda { |x| ramlrb_parse(x) }
-  # end
-
-  parser_func = PARSERS[options.parser]
-  puts parser_func
 
   ex_dir = clone_tck_repo()
   puts ex_dir
@@ -31,7 +25,7 @@ def main()
   total = files_list.length
   files_list.each do |fpath|
     puts "> Parsing #{fpath}:"
-    parser_func(fpath)
+    parse_with(options.parser, fpath)
   end
 
   puts "\nPassed/Total: #{passed}/#{total}"
